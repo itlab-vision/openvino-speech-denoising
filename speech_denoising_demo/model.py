3842890266e031179a9516edf588bf00509b60b7
ivanvikhrev <l4ind@mail.ru>
2020-09-17 11:38:00 +0300

    create project structure

---
+++ speech_denoising_demo/model.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+import sys, os
+import numpy as np
+from openvino.inference_engine import IENetwork, IECore
+import features
+
+input_layer_name = "input"
+
+class SpeechDenoiser(ABC):
+     @staticmethod
+     def create(args):
+        if args['name'] == 'DNS':
+            return DNS(args['xml'])
+        else:
+            raise Exception('Error: wrong name')
+
+     @abstractmethod
+     def denoise(self, img, ID):
+         '''Perform Noise Suppression'''
+
+class DNS(SpeechDenoiser):
+    def __init__(self, model):
+        ''''''
+
+    def denoise(self, sigIn, inFs):
+        ''''''
