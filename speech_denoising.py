e6a1d4f271b31f6f6113514e9ce87f450b2b058f
Aleksei <aleksei.korobeinikov@intel.com>
2020-09-26 18:37:18 +0300

    Add inference for constant shape of inputs

--- speech_denoising.py
+++ speech_denoising.py
@@ -1,16 +1,19 @@
 import sys
 import argparse
 sys.path.insert(0, 'src')
-import model as m
+from speech_denoising_demo.model import SpeechDenoiser
+import logging as log
 import soundfile as sf
 
 def build_argparser():
-    parser = argparse.ArgumentParser(description='Speech denoising demo')
+    parser = argparse.ArgumentParser(description='Speech denoising demo', add_help=False)
     parser.add_argument('-h', '--help', action='help', default = argparse.SUPPRESS,
                         help = 'Show this help message and exit.')
+    parser.add_argument('-m', '--model', type=str, required = True,
+                        help = 'Required. Path to model (.xml file)')
     parser.add_argument('-i', '--input', type = str, required = True,
                         help = 'Required. Path to audiofile with noisy speech.')
-    parser.add_argument('-o', '--output', type = str, required = True, default = './',
+    parser.add_argument('-o', '--output', type = str, required = False, default = './',
                         help = 'Optional. Path to output dir.')
     parser.add_argument('-d', '--device', type=str, default='CPU',
                         help = 'Optional. Specify a target device to infer on. CPU, GPU, FPGA, HDDL or MYRIAD is '
@@ -21,7 +24,25 @@ def build_argparser():
     return args
 
 def main():
-    args = build_argparser().parse_args()
+    log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
+    args = vars(build_argparser())
+    args['name'] = 'DNS'
+
+    log.info("Creating SpeechDenoising Model")
+    model = SpeechDenoiser.create(args)
+    
+    log.info("Reading input data")
+    if '.wav' in args['input']:
+        # input is single .wav file
+        sound, fs = sf.read(args['input'])
+        print(sound.shape)
+
+    log.info("Inference for input")
+    out = model.denoise(sound)
+
+    log.info("Save the result")
+    sf.write(args['output'] + "/result.wav", out, fs)
+
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
--- speech_denoising_demo/model.py
+++ speech_denoising_demo/model.py
@@ -1,24 +1,65 @@
 from abc import ABC, abstractmethod
 import sys, os
 import numpy as np
-from openvino.inference_engine import IENetwork, IECore
-import features
+from .features import calcSpec, calcFeat, spec2sig
+from openvino.inference_engine import IECore
 
 class SpeechDenoiser(ABC):
-     @staticmethod
-     def create(args):
-        if args['name'] == 'DNS':
-            return DeepNoiseSuppression(args['xml'])
-        else:
-            raise Exception('Error: wrong name')
+    ie_core = IECore()
 
-     @abstractmethod
-     def denoise(self, img, ID):
-         '''Perform Noise Suppression'''
+    @staticmethod
+    def create(args):
+       if args['name'] == 'DNS':
+           return DeepNoiseSuppression(args['model'], args['device'])
+       else:
+           raise Exception('Error: wrong name')
+    @abstractmethod
+    def denoise(self, img, ID):
+        '''Perform Noise Suppression'''
 
 class DeepNoiseSuppression(SpeechDenoiser):
-    def __init__(self, model):
-        ''''''
+    def __init__(self, model, device='CPU'):
+        print("Parameters for model ....")
+        self.cfg = {
+            'winlen'   : 0.02,
+            'hopfrac'  : 0.5,
+            'fs'       : 16000,
+            'mingain'  : -80,
+            'feattype' : 'LogPow'
+        }
+        self.mingain = 10**(self.cfg['mingain']/20)
 
-    def denoise(self, sigIn, inFs):
-        ''''''
+        print("Reading IR for model ....")
+        self.network = SpeechDenoiser.ie_core.read_network(model, model.replace(".xml", ".bin"))
+        self.input_blob = next(iter(self.network.input_info))
+        self.output_blob = 'output'
+        assert len(self.network.input_info) == 1, "One input is expected"
+
+        print("Loading network to the plugin...")
+        self.exec_net = SpeechDenoiser.ie_core.load_network(network=self.network, device_name=device)
+    
+    def preprocessing(self, sigIn):
+        self.inputSpec = calcSpec(sigIn, self.cfg)
+        inputFeature = calcFeat(self.inputSpec, self.cfg)
+        # shape: [batch x time x freq]
+        inputFeature = np.expand_dims(np.transpose(inputFeature), axis=0)
+        return inputFeature
+
+    def postprocessing(self, out):
+        Gain = np.transpose(out)
+        Gain = np.clip(Gain, a_min=self.mingain, a_max=1.0)
+        outSpec = np.expand_dims(self.inputSpec, axis=2) * Gain
+
+        # go back to time domain
+        sigOut = spec2sig(outSpec, self.cfg)
+        return sigOut
+
+    def denoise(self, sigIn):
+        inputFeature = self.preprocessing(sigIn)
+
+        # Calculate network output
+        out = self.exec_net.infer({self.input_blob: inputFeature})[self.output_blob]
+        # print(out)
+        print(out.shape)
+        result = self.postprocessing(out)
+        return result
