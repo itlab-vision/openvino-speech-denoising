f6290264da1aa560bcd4f539d2e6375dbee092d2
ivanvikhrev <l4ind@mail.ru>
2020-09-26 21:27:11 +0300

    raw implemetation

--- speech_denoising.py
+++ speech_denoising.py
@@ -1,27 +1,64 @@
+import logging as log
+import os
 import sys
+import time
 import argparse
-sys.path.insert(0, 'src')
-import model as m
+
 import soundfile as sf
+import sounddevice as sd
+
+from openvino.inference_engine import IECore
+from speech_denoising_demo.model import SpeechDenoiser
 
 def build_argparser():
-    parser = argparse.ArgumentParser(description='Speech denoising demo')
-    parser.add_argument('-h', '--help', action='help', default = argparse.SUPPRESS,
-                        help = 'Show this help message and exit.')
-    parser.add_argument('-i', '--input', type = str, required = True,
-                        help = 'Required. Path to audiofile with noisy speech.')
-    parser.add_argument('-o', '--output', type = str, required = True, default = './',
+    parser = argparse.ArgumentParser(description='Speech denoising demo', add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                      help='Show this help message and exit.')
+    args.add_argument('-m', '--model', type=str, required=True,
+                        help= 'Required. Path to .XML file with pre-trained model.')
+    args.add_argument('-i', '--input', type = str, required = True,
+                        help = 'Required. Audiofile to process. Default value is "mic" to work with default microphone')
+    args.add_argument('-o', '--output', type = str, default = './',
                         help = 'Optional. Path to output dir.')
-    parser.add_argument('-d', '--device', type=str, default='CPU',
+    args.add_argument('-d', '--device', type = str, default='CPU',
                         help = 'Optional. Specify a target device to infer on. CPU, GPU, FPGA, HDDL or MYRIAD is '
                              'acceptable. The demo will look for a suitable plugin for the device specified. '
                              'Default value is CPU')
+    args.add_argument('-no_play',  action='store_true', help = 'Optional. Do not play inference results.')
 
-    args = parser.parse_args()
-    return args
+    return parser
+
+#def preprocess_audio(input)
 
 def main():
+    """ Main function. """
+    log.basicConfig(format='[ %(levelname)s ] %(message)s', level= log.INFO, stream=sys.stdout)
     args = build_argparser().parse_args()
+    log.info('Create denoiser')
+    denoiser = SpeechDenoiser.create(args)
+    fs = 16000 # default sample rate for input
+    if args.input == 'mic':
+        log.info('Start recording')
+        duration = 10  # seconds
+        data = sd.rec(int(duration * fs), samplerate = fs, channels=1)
+        sd.wait()
+        log.info('Saving recording')
+        sf.write('recording.wav', data, fs)
+        log.info('Recording was written to ' + str(os.path.abspath(os.getcwd())) + '\\recording.wav')
+    else:
+        data, fs = sf.read(args.input)
+        print(fs)
+
+    res = denoiser.denoise(data)
+    log.info('Playing result')
+    sd.play(res, fs)
+    status = sd.wait()
+
+    log.info('Saving result')
+    sf.write('res.wav', res, fs)
+    log.info('Result was written to ' + str(os.path.abspath(os.getcwd())) + '\\res.wav')
+    # at least two cases : 1) upload audio, process it an than play. 2) record, process, play
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
--- speech_denoising_demo/model.py
+++ speech_denoising_demo/model.py
@@ -1,24 +1,63 @@
+import logging as log
 from abc import ABC, abstractmethod
-import sys, os
+import sys
+import os
 import numpy as np
+
+from .features import calcFeat, calcSpec, spec2sig
 from openvino.inference_engine import IENetwork, IECore
-import features
 
 class SpeechDenoiser(ABC):
      @staticmethod
      def create(args):
-        if args['name'] == 'DNS':
-            return DeepNoiseSuppression(args['xml'])
+        if args.model == 'nsnet2-20ms-baseline.xml':
+            return DeepNoiseSuppression(args.model, args.device)
         else:
             raise Exception('Error: wrong name')
 
      @abstractmethod
-     def denoise(self, img, ID):
+     def denoise(self, data):
          '''Perform Noise Suppression'''
 
 class DeepNoiseSuppression(SpeechDenoiser):
-    def __init__(self, model):
-        ''''''
+    
+    def __init__(self, model, device):
+        log.basicConfig(format='[ %(levelname)s ] %(message)s', level= log.INFO, stream=sys.stdout)
+        # Plugin initialization
+        ie = IECore()
+        log.info("Loading network")
+        net = ie.read_network(model, os.path.splitext(model)[0] + ".bin")
+        # Loading model to the plugin
+        log.info("Loading model to the plugin")
+        self.exec_net = ie.load_network(network=net, device_name= device)
 
-    def denoise(self, sigIn, inFs):
-        ''''''
+    def denoise(self, data):
+        self.cfg = {
+            'winlen'   : 0.02,
+            'hopfrac'  : 0.5,
+            'fs'       : 16000,
+            'mingain'  : -80,
+            'feattype' : 'LogPow'
+        }
+        self.frameShift = float(self.cfg['winlen'])* float(self.cfg["hopfrac"])
+        self.fs = int(self.cfg['fs'])
+        self.Nfft = int(float(self.cfg['winlen'])*self.fs)
+        self.mingain = 10**(self.cfg['mingain']/20)
+        """Enhance a single Audio signal."""
+        log.info("Calculate features for audio signal")
+        inputSpec = calcSpec(data, self.cfg)
+        inputFeature = calcFeat(inputSpec, self.cfg)
+        inputFeature = np.expand_dims(np.transpose(inputFeature), axis=0)
+
+        log.info("Starting inference")
+        out = self.exec_net.infer({'input': inputFeature})
+        log.info("Inference successfull")
+        log.info("Processing output")
+        res = out['Sigmoid_31'][0]
+        Gain = np.transpose(res)
+        Gain = np.clip(Gain, a_min=self.mingain, a_max=1.0)
+        outSpec = inputSpec * Gain
+        # go back to time domain
+        out = spec2sig(outSpec, self.cfg)
+        log.info("Processing sucessfull")
+        return out
